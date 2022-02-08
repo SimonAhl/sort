c2e650eaa2c70f0fca4e9a9b778a0bf19cb93083
naal01002 <89377694+naal01002@users.noreply.github.com>
2022-02-08 14:21:55 +0100

    g

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace dort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> intList = new List<int>();
+
+            Random ran = new Random();
+
+            Stopwatch watch = new Stopwatch();
+
+            for(int i = 0; intList.Count < 1000; i++){
+                intList.Add(ran.Next(1, 1001));
+            }
+
+            watch.Start();
+
+            for(int i = 0; i < intList.Count- 1; i++){
+                for(int j = 0; j < intList.Count -1; j++){
+                    if(intList[j] > intList[j+1]){
+                        int a = intList[j];
+                        intList[j] = intList[j + 1];
+                        intList[j+1] = a;
+                    }
+                }
+            }
+
+            watch.Stop();
+    
+            foreach(int i in intList){
+                Console.WriteLine(i);
+            }
+
+            Console.WriteLine(watch.ElapsedMilliseconds + " ms");
+        }
+    }
+}
